852c87e4b15c289262108d71d43d586f4ea08e1e
Roshiajin <fdreamer@ya.ru>
2016-08-31 14:13:32 +0300

    edit check

--- src/com/epam/testtasks/DataChecker.java
+++ src/com/epam/testtasks/DataChecker.java
@@ -3,16 +3,16 @@ package com.epam.testtasks;
 import java.util.List;
 
 public class DataChecker {
-	private ItemRepo itemRepo;
+	private ItemRepo itemRepo = new Repo();
 	
 	public boolean isDataChanged(List itemsFromUI) {
 	/* Implement me*/
-	itemRepo = new Repo();
-	
-	return itemsFromUI.containsAll(itemRepo.getItems()) && itemRepo.getItems().containsAll(itemsFromUI);
-		
-	//return true;
+
+        if (itemsFromUI.size() != itemRepo.getItems().size()) {
+            return false;
+        }
+        else return itemsFromUI.containsAll(itemRepo.getItems());
+
 	}
-	
 	
 }
